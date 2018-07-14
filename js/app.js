7698403838b2060ce685d0ee282560a11218ac27
Nagashree <nagashreemylaraiah@gmail.com>
2018-07-13 18:57:39 -0700

    Implement Game Win scenario

--- js/app.js
+++ js/app.js
@@ -5,19 +5,13 @@ class Enemy{
             this.x = x;
             this.y = y;
             this.step = [100,80];
+            this.speed = 50;
         }
     update(dt){
-        // Update the enemy's position, required method for game
-        // Parameter: dt, a time delta between ticks
-        // You should multiply any movement by the dt parameter
-        // which will ensure the game runs at the same speed for
-        // all computers.
-        this.x= (this.x>400)? 0: (this.x+(50*dt));
+        this.x= (this.x>400)? 0: (this.x+(this.speed*dt));
         this.position = [Math.round(this.x/this.step[0]),Math.round(this.y/this.step[1])];
-        //console.log(`enemy position: ${this.position}`);
     }
     render(){
-        // Draw the enemy on the screen, required method for game
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
 
@@ -32,6 +26,7 @@ class Player{
         this.y= y;
         this.step = [100,80];
         this.move = [0,0];
+        this.win = false;
     }
     update(){
         this.position = [(this.x/this.step[0]),(this.y/this.step[1])];
@@ -41,6 +36,24 @@ class Player{
     }
     render(){
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+        if (this.win){
+            console.log(this.win);
+            ctx.font = "36pt Impact";
+            ctx.textAlign = "center";
+            ctx.fillStyle = "white";
+            ctx.fillText("WELL DONE!",player.step[0]*2.5,player.step[1]*3);
+            ctx.strokeStyle = "black";
+            ctx.lineWidth = 3;
+            ctx.strokeText("WELL DONE!",player.step[0]*2.5,player.step[1]*3);
+
+            ctx.font = "20pt Impact";
+            ctx.textAlign = "center";
+            ctx.fillStyle = "white";
+            ctx.fillText("Press ENTER to play again!",player.step[0]*2.5,player.step[1]*4);
+            ctx.strokeStyle = "black";
+            ctx.lineWidth = 2;
+            ctx.strokeText("Press ENTER to play again!",player.step[0]*2.5,player.step[1]*4);
+        }
     }
 
     handleInput(keypress){
@@ -73,11 +86,8 @@ class Player{
 };
 
     function checkForCollision(){
-        //console.log(`checking for collision`);
         for(let enemy of allEnemies) {
-            //console.log(`enemy: ${enemy.position} player: ${player.position}`);
             if((player.position[0]===enemy.position[0]) && (player.position[1]===enemy.position[1])){
-                //console.log(`enemy: ${enemy.x},${enemy.y} player: ${player.x}, ${player.y}`);
                 resetPlayer();
             }
         }
@@ -86,24 +96,18 @@ class Player{
     function checkForWin(){
         console.log(`checking for win ${player.position[1]}`);
         if (player.position[1]===0){
-               /*  ctx.font = "36pt Impact";
-                ctx.textAlign = "center";
-                ctx.fillStyle = "white";
-                ctx.fillText("WELL DONE!",player.step[0]*2.5,player.step[1]*4);
-                ctx.strokeStyle = "black";
-                ctx.lineWidth = 3;
-                ctx.strokeText("WELL DONE!",player.step[0]*2.5,player.step[1]*4);  */
-                resetPlayer();
-            
+            player.win = true;
+            for(let enemy of allEnemies){
+                enemy.speed=0;
+            }
+            console.log('cplayer position is top row');
         }
     };
 
     function resetPlayer(){
         [player.x,player.y]= [2*player.step[0],5*player.step[1]];
     }
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+
 const allEnemies = [new Enemy(100,65),new Enemy(200,145), new Enemy(300,225)];
 const player = new Player();
 
@@ -117,4 +121,14 @@ document.addEventListener('keyup', function(e) {
         40: 'down'
     };
     player.handleInput(allowedKeys[e.keyCode]);
+});
+
+document.addEventListener('keypress', function(e) {
+    if(e.keyCode ===13){
+        resetPlayer();
+        player.win = false;
+        for (let enemy of allEnemies){
+            enemy.speed=50;
+        }
+    }
 });
