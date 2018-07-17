7094389a64113635adc88aba02ddc90a8a7bd90a
Nagashree <nagashreemylaraiah@gmail.com>
2018-07-16 23:23:06 -0700

    Refactor app.js, Add documentation

--- js/app.js
+++ js/app.js
@@ -1,125 +1,204 @@
+/** @description Store the values corresponding to rows and columns of the Canvas
+    @constant
+    @maxX and maxY store total number of blocks per row and column respectively
+    @pixelsPerX and pixelsPerY store the number of pixels per block 
+    @type {number}
+*/
+const maxX = 4;
+const maxY = 5;
+const pixelsPerX = 100;
+const pixelsPerY= 80;
 
-class Character{
-    constructor(x=0, y=0, sprite){
-            this.sprite = sprite;
-            this.x = x;
-            this.y = y;
-            this.step = [100,80];
-            this.position = [];
-        }
-    update(){
-        this.position = [Math.round(this.x/this.step[0]),Math.round(this.y/this.step[1])];
+/** @description True if the Player has won
+    @variable
+    @type {number}
+*/
+let win = false;
+
+/**
+ * @description Creates a new Enemy
+ * @constructor
+ * @param {number} x coordinate on the canvas for the placement of the object
+ * @param {number} y coordinate on the canvas for the placement of the object
+ * @param {string} sprite - image(png) to be the object
+ * @param {number} speed - speed with which the object should be moved along the canvas
+ */
+class Enemy{
+    constructor(x, y, sprite ='images/enemy-bug.png', speed = 50){
+        this.x = x;
+        this.y = y;
+        this.sprite = sprite;
+        this.speed = speed;
     }
+
+/**
+* @description Updates the position of the Enemy object
+* @param {number} dt - a time delta between ticks
+* If the Enemy Object has reached the end of the screen, start from the start of the screen
+* Compute the position in terms of blocks represented by posX and posY of the Enemy on the screen
+*/
+    update(dt){
+        this.x+= this.speed*dt;
+        if (this.x > maxX*pixelsPerX){
+            this.x = 0;
+        } 
+        this.posX = Math.round(this.x/pixelsPerX);
+        this.posY = Math.round(this.y/pixelsPerY);
+    }
+
+/**
+* @description Draws the Enemy object on the canvas
+* @param none
+*/
+    render(){
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+};
+
+/**
+ * @description Creates a new Player
+ * @constructor
+ * @param {number} x coordinate on the canvas for the placement of the object
+ * @param {number} y coordinate on the canvas for the placement of the object
+ * @param {string} sprite - image(png) to be the object
+ */
+class Player {
+    constructor(x = 2*pixelsPerX, y = 5*pixelsPerY, sprite = 'images/char-princess-girl.png'){
+       this.x = x;
+       this.y = y;
+       this.sprite = sprite;
+    }
+
+/**
+* @description Updates the position of the Player object
+* @param none
+* Compute the position in terms of blocks represented by posX and posY of the Player on the screen
+* Check if the Player collides with any of the Enemies
+* Check if the Player has reached the top row(water)
+*/
+    update(){
+        this.posX = Math.round(this.x/pixelsPerX);
+        this.posY = Math.round(this.y/pixelsPerY);
+        checkForCollision();
+        checkForWin();
+    }
+
+/**
+* @description Draws the Player object on the canvas
+* @param none
+*/
     render(){
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
 
-}
-
-class Enemy extends Character{
-    constructor(x,y,sprite){
-        super(x,y,sprite='images/enemy-bug.png');
-        this.speed = 50;
+/**
+* @description Resets the position of the Player
+* @param none
+*/
+    reset(){
+        this.x = 2*pixelsPerX;
+        this.y = 5*pixelsPerY;
     }
 
-    update(dt){
-        super.update();
-        this.x= (this.x>400)? 0: (this.x+(this.speed*dt));
-    }
-
-    render(){
-        super.render();
-
-    }
-
-}
-
-class Player extends Character{
-    constructor(x=200,y=400,sprite ='images/char-princess-girl.png' ){
-        super(x,y,sprite);
-        this.move = [0,0];
-        this.win = false;
-    }
-
-    update(){
-        super.update();
-        this.checkForCollision();
-        this.checkForWin();
-    }
-
-    render(){
-        super.render();
-        if (this.win){
-            ctx.font = "36pt Impact";
-            ctx.textAlign = "center";
-            ctx.fillStyle = "white";
-            ctx.fillText("WELL DONE!",player.step[0]*2.5,player.step[1]*3);
-            ctx.strokeStyle = "black";
-            ctx.lineWidth = 3;
-            ctx.strokeText("WELL DONE!",player.step[0]*2.5,player.step[1]*3);
-
-            ctx.font = "20pt Impact";
-            ctx.textAlign = "center";
-            ctx.fillStyle = "white";
-            ctx.fillText("Press ENTER to play again!",player.step[0]*2.5,player.step[1]*4);
-            ctx.strokeStyle = "black";
-            ctx.lineWidth = 2;
-            ctx.strokeText("Press ENTER to play again!",player.step[0]*2.5,player.step[1]*4);
-        }
-    }
-
+/**
+* @description Moves the Player object on the canvas based on the keyboard input
+* @param {key} Arrow keys 
+* If the Player has reached the end of the screen, do not update the position
+*/
     handleInput(keyInput){
         switch(keyInput){
-
             case 'left':
-                this.move = ((this.x===0)?[0,0]:[-1,0]);
+                if(this.x!==0){
+                    this.x-= pixelsPerX;
+                }
                 break;
        
             case 'up':
-                this.move = ((this.y===0)?[0,0]:[0,-1]);
+                if(this.y!==0){
+                    this.y-= pixelsPerY;
+                }
                 break;
         
             case 'right':
-                this.move= ((this.x===this.step[0]*4)?[0,0]:[1,0]);
+                if(this.x!==pixelsPerX*maxX){
+                    this.x+= pixelsPerX;
+                }
                 break;
             
             case 'down':
-                this.move = ((this.y===this.step[1]*5)?[0,0]:[0,1]);
+                if(this.y!==pixelsPerY*maxY){
+                    this.y+= pixelsPerY;
+                }
                 break;
         }
-
-        this.x+= this.step[0]*this.move[0];
-        this.y+= this.step[1]*this.move[1];
-    }
-
-    checkForCollision(){
-        for(let enemy of allEnemies) {
-            if((this.position[0]===enemy.position[0]) && (this.position[1]===enemy.position[1])){
-                this.resetPlayer();
-            }
-        }
-    }
-    
-    checkForWin(){
-        if (this.position[1]===0){
-            this.win = true;
-            for(let enemy of allEnemies){
-                enemy.speed=0;
-            }
-        }
-    };
-
-    resetPlayer(){
-        [this.x,this.y]= [2*this.step[0],5*this.step[1]];
-        this.move = [0,0];
     }
 };
 
-const allEnemies = [new Enemy(100,65),new Enemy(200,145), new Enemy(300,225)];
-const player = new Player();
+/**
+* @description Checks if the Player object collides with the Enemy object
+* Check if the Player object and any Enemy object are in the same position
+* Reset the Player position back to start upon collision
+* @param none
+*/
+function checkForCollision(){
+    for(let enemy of allEnemies) {
+        if((enemy.posX===player.posX) && (enemy.posY===player.posY)){
+            player.reset();
+        }
+    }
+}
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+/**
+* @description Renders the 'Winning Message' on the canvas when the Player object wins
+* @param none
+*/    
+function renderWin(){
+    if(win){
+        ctx.font = "36pt Impact";
+        ctx.textAlign = "center";
+        ctx.fillStyle = "white";
+        ctx.fillText("WELL DONE!", pixelsPerX*2.5, pixelsPerY*3);
+        ctx.strokeStyle = "black";
+        ctx.lineWidth = 3;
+        ctx.strokeText("WELL DONE!", pixelsPerX*2.5, pixelsPerY*3);
+
+        ctx.font = "20pt Impact";
+        ctx.textAlign = "center";
+        ctx.fillStyle = "white";
+        ctx.fillText("Press ENTER to play again!", pixelsPerX*2.5, pixelsPerY*4);
+        ctx.strokeStyle = "black";
+        ctx.lineWidth = 2;
+        ctx.strokeText("Press ENTER to play again!", pixelsPerX*2.5, pixelsPerY*4);
+    }
+}
+
+/**
+* @description Checks if the Player object has won
+* Check if the Player has reached the top most row
+* If the Player has won, pause the enemies and render the Win message (invoked from engine.js)
+* @param none
+*/
+function checkForWin(){
+    if (player.posY===0){
+        for(let enemy of allEnemies){
+            enemy.speed = 0;
+        }
+        win = true;
+    }
+}
+
+/** 
+* Instantiate the Player and Enemy objects
+* Create multiple enemy objects and place them in an array allEnemies
+*/
+const player = new Player();
+const allEnemies = [new Enemy(100,65), new Enemy(200,145), new Enemy(300,225)];
+
+
+/**
+* Listen for key presses and send the keys to the
+* Player.handleInput() method.
+*/
 document.addEventListener('keyup', function(e) {
 
     var allowedKeys = {
@@ -131,12 +210,16 @@ document.addEventListener('keyup', function(e) {
     player.handleInput(allowedKeys[e.keyCode]);
 });
 
+/**
+* Listen for 'Enter' key press 
+* Reset the position of the Player and speed of the Enemy and restart the game 
+*/
 document.addEventListener('keypress', function(e) {
-    if(e.keyCode ===13){
-        player.resetPlayer();
-        player.win = false;
-        for (let enemy of allEnemies){
-            enemy.speed=50;
-        }
+    if(e.keyCode===13){
+        player.reset();
+        for(let enemy of allEnemies){
+            enemy.speed = 50;
+        } 
+        win = false;       
     }
 });
